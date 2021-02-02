df0cfe0eb9bb58776a1874581275049fdaf7ab60
Rafaela Prendi <rafaelaprendi7@gmail.com>
2021-02-02 12:41:58 +0100

    finished controllers

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -31,7 +31,44 @@ module.exports = {
             res.status(500).json({ message: "Server has an error" })
         })
     },
-    update: (req, res, next) => {},
-    create: (req, res, next) => {},
+    update: (req, res, next) => {
+        const body = req.body
+        console.log("new data", body)
+        if (body.name === undefined) {
+            return response.status(400).json({ error: 'content missing' });
+        }
+        const course = {
+            name: body.name,
+            description: body.description,
+            hours: body.hours,
+            students: body.students
+        }
+        Course.findByIdAndUpdate(req.params.id, course, { new: true })
+            .then(updatedCourse => {
+                res.json(updatedCourse)
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({ message: "Server has an error" })
+            })
+    },
+    create: (req, res, next) => {
+        console.log(req.body, "body");
+        const body = req.body;
+
+        if (body.name === undefined) {
+            return response.status(400).json({ error: 'content missing' });
+        }
+
+        const newCourse = new Course({
+            name: body.name,
+            description: body.description,
+            hours: body.hours,
+            students: body.students
+        });
+        newCourse.save().then(savedCourse => {
+            res.status(200).json(savedCourse)
+        });
+    },
 
 }
--- controllers/teacher.controller.js
+++ controllers/teacher.controller.js
@@ -31,7 +31,42 @@ module.exports = {
             res.status(500).json({ message: "Server has an error" })
         })
     },
-    update: (req, res, next) => {},
-    create: (req, res, next) => {},
+    create: (req, res, next) => {
+        console.log(req.body, "body");
+        const body = req.body;
+
+        if (body.name === undefined) {
+            return response.status(400).json({ error: 'content missing' });
+        }
+        const newTeacher = new Teacher({
+            name: body.name,
+            subject: body.subject,
+            contact: body.contact,
+            courses: body.courses
+        });
+        newTeacher.save().then(savedTeacher => {
+            res.status(200).json(savedTeacher)
+        });
+
+    },
+    update: (req, res, next) => {
+        const body = req.body
+        console.log("new data", body);
+        const teacher = {
+            name: body.name,
+            subject: body.subject,
+            contact: body.contact,
+            courses: body.courses
+        }
+
+        Teacher.findByIdAndUpdate(req.params.id, teacher, { new: true })
+            .then(updatedTeacher => {
+                res.json(updatedTeacher)
+            })
+            .catch(error => {
+                console.log(error)
+                res.status(500).json({ message: "Server has an error" })
+            })
+    },
 
 }
